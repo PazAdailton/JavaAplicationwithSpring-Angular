db012ab9fd843419744ae8db239e9995833dc802
Adailton Paz <adailtonpaz@pazimports.onmicrosoft.com>
2025-01-01 22:02:23 -0300

    Primeiro commit

---
+++ Documents/FrontAngularJS/Angular/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
+@NgModule({
+  declarations: [ /* seus componentes aqui */ ],
+  imports: [
+    NgxDatatableModule,
+    /* outros módulos */
+  ],
+  bootstrap: [/* componente raiz */]
+})
+export class AppModule { }
